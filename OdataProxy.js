4cc944628856793701286a64a86871b034f8bb1a
Luc Stakenborg <l.stakenborg@oxida.com>
2012-01-11 16:33:11 +0100

    Initial commit

---
+++ OdataProxy.js
@@ -0,0 +1,203 @@
+﻿/******************************************************************
+ *  Ext.ux.ODataProxy 
+ * 
+ *  Author: Luc Stakenborg
+ *  Date: Jan 11, 2012
+ * 
+ *  Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php 
+ *  Copyright (c) 2012, Luc Stakenborg, Oxida B.V.
+ ******************************************************************
+ */
+
+
+
+
+// Sencha Touch 1.x doesn't implement the model.destroy() method,
+// so we define it here in an override.
+// The code is adapted from the save method.
+Ext.override(Ext.data.Model, {
+  destroy: function (options) {
+    var me = this,
+        action = 'destroy';
+
+    options = options || {};
+
+    Ext.apply(options, {
+      records: [me],
+      action: action
+    });
+
+    var operation = new Ext.data.Operation(options),
+        successFn = options.success,
+        failureFn = options.failure,
+        callbackFn = options.callback,
+        scope = options.scope,
+        record;
+
+    var callback = function (operation) {
+        record = operation.getRecords()[0];
+
+      if (operation.wasSuccessful()) {
+
+
+        if (typeof successFn == 'function') {
+          successFn.call(scope, record, operation);
+        }
+      } else {
+        if (typeof failureFn == 'function') {
+          failureFn.call(scope, record, operation);
+        }
+      }
+
+      if (typeof callbackFn == 'function') {
+        callbackFn.call(scope, record, operation);
+      }
+    };
+
+    me.getProxy()[action](operation, callback, me);
+
+    return me;
+  }
+
+})
+
+
+// OData requires If-Match headers for MERGE/PUT requests
+Ext.Ajax.on('beforerequest', function (conn, options) {
+  options.headers = options.headers || {};
+  if (options.etag) {
+    // if etag is provided, add an If-Match HTTP header
+    options.headers['If-Match'] = options.etag
+  } else {
+    // if no etag is provided, remove the If-Match HTTP header again
+    delete options.headers['If-Match']
+  }
+}, this);
+
+
+Ext.ux.ODataProxy = Ext.extend(Ext.data.RestProxy, {
+
+  actionMethods: {
+    create: 'POST',
+    read: 'GET',
+    // Please note: for updates, MERGE is used instead of PUT.
+    // PUT expects all fields to be provided, resetting non-provided field to default values.
+    // MERGE will only update provided fields
+    update: 'MERGE',   
+    destroy: 'DELETE'
+  },
+
+  // required, to tell OData service to respond in JSON format
+  headers: {
+    'Accept': 'application/json'
+  },
+
+  // don't use cache busting
+  noCache: false,
+
+  limitParam: undefined,
+
+  // provide OData style RESTful urls
+  // .../resouce(id) instead of ../resource/id 
+  buildUrl: function (request) {
+    var records = request.operation.records || [],
+            record = records[0],
+            url = request.url || this.url,
+            id = record ? record.getId() : request.operation.id;
+
+    if (id) {
+      url += '(' + id + ')';
+    }
+
+    request.url = url;
+
+    return Ext.data.RestProxy.superclass.buildUrl.apply(this, arguments);
+  },
+
+  writer: {
+    type: 'json',
+    writeRecords: function (request, data) {
+      var record = request.records[0],
+          etag;
+
+
+      // We need pass the etag header required for the update (MERGE). 
+      // The Ext.Ajax beforerequest event creates an 'If-Match' http header if etag is present in request.
+      if (request.action == "update") {
+        etag = record.raw.__metadata.etag;
+        request.etag = etag ;
+      }
+
+      // changed to turn data array in single record
+      request.jsonData = data[0];
+
+      // override the getRecords function of operation
+      request.operation.getRecords = function () {
+        var resultSet = this.getResultSet();
+
+        // if a nullResultSet is returned, use this.records
+        return (resultSet == undefined || resultSet.count == 0 ? this.records : resultSet.records);
+      };
+
+      return request;
+    }
+  },
+
+
+  reader: {
+    type: 'json',
+    root: 'd',
+
+    // override to deal with 204 responses (NO CONTENT)
+    read: function (response) {
+      var data = response;
+
+      if (response && response.responseText) {
+        data = this.getResponseData(response);
+      }
+
+      // MERGE and DELETE respond with a 204 NO CONTENT
+      // In this case the responseText is an empty string
+      if (response && response.responseText == "") {
+        data = null;
+      }
+
+      if (data) {
+        return this.readRecords(data);
+      } else {
+        return this.nullResultSet;
+      }
+    },
+
+    getData: function (response) {
+
+      if (response && response.d && response.d.results) {
+        response.d = response.d.results
+      }
+
+      // RestProxy always expects an array of results,
+      // even if there is just a single record.
+      if (!Ext.isArray(response.d)) {
+        response.d = [response.d]
+      }
+
+      return response;
+
+    }
+  }
+});
+
+
+// finally, register the proxy type 'odata'
+Ext.data.ProxyMgr.registerType('odata', Ext.ux.ODataProxy);
+
+
+
+
+
+
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,65 @@
+﻿Ext.ux.ODataProxy
+===================
+
+Backbone.SharePoint extends Backbone Models and Collections so you can easily work with SharePoint Items and Lists.
+Its custom sync() method is a wrapper around the SharePoint ListData.svc REST service which is based on [OData](http://www.odata.org). 
+
+
+Backbone.SharePoint features:
+
+* create, read, update and delete SharePoint items as Backbone models
+* fetch multiple SharePoint items from a list as a Backbone collection
+* support for OData query string options (orderby, filter, select, etc)
+* JSON payloads
+* custom sync() to communicate with the ListData service
+* partial updates: only changed attributes are sent to the server during an update
+* lightweight
+
+
+
+Contents
+--------
+* [Getting started](#installation)
+* [Examples](#examples)
+* [Tests](#tests)
+
+
+Getting started
+---------------
+Because of the Same Origin Policy, your html file must served from the same domain as the SharePoint site you want to access. 
+You can place your html file containing your app on the server file system or in an asset library.  
+
+
+index.html:
+ 
+```html
+
+
+<!doctype html>
+<html>
+   ....
+<script src="jquery.js"></script> 
+<!-- you can also use zepto.js -->
+
+<script src="underscore.js"></script>
+<script src="backbone.js"></script>
+<script src="backbone-sharepoint.js"></script>
+  ...
+
+</html>
+
+```
+
+## <a name="examples"/>Examples
+
+Now let's look at some examples how you can use Backbone.SharePoint. In these examples we will assume you have 
+a subsite '/teamsite' in which you have 
+created a Contacts list based on the standard contacts list template. 
+
+
+
+Hopefully this is sufficient to get you going!
+
+
+
+
